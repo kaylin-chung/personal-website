ce399fa4b8b0ebf67136f4a920f0d7482bc54064
Kaylin Chung <kay1inchung@g.ucla.edu>
2023-07-14 12:47:26 -0700

    updated resume

--- src/containers/resume/index.jsx
+++ src/containers/resume/index.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import PageHeaderContent from "../../components/pageHeaderContent";
 import { AiOutlinePaperClip } from 'react-icons/ai';
-import kaylinResume from "./kaylinsResume.pdf";
+import kaylinResume from "./kaylinchung-fall2023.pdf";
 import "./styles.scss";
 
 
