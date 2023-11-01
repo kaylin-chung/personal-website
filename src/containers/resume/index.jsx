dbe95ef9ac51e112f7ec9ebf015a759eae8aea7c
Kaylin Chung <kay1inchung@g.ucla.edu>
2023-11-01 14:34:44 -0700

    updated

--- src/containers/resume/index.jsx
+++ src/containers/resume/index.jsx
@@ -1,24 +1,21 @@
 import React from "react";
 import PageHeaderContent from "../../components/pageHeaderContent";
-import { AiOutlinePaperClip } from 'react-icons/ai';
-import kaylinResume from "./kaylinchung-fall2023.pdf";
+import { AiOutlinePaperClip } from "react-icons/ai";
+import kaylinResume from "./KaylinChungFall2023.pdf";
 import "./styles.scss";
 
-
 const Resume = () => {
-    return (
-        <section className="resume" id="resume">
-            <PageHeaderContent
-                headerText="my resume"
-                icon={<AiOutlinePaperClip size={40} />}
-            />
-            <div className="resume__resDoc">
-                <embed src={kaylinResume} width="900px" height="970px" />
-            </div>
-            
-        </section>
-
-    );
+  return (
+    <section className="resume" id="resume">
+      <PageHeaderContent
+        headerText="my resume"
+        icon={<AiOutlinePaperClip size={40} />}
+      />
+      <div className="resume__resDoc">
+        <embed src={kaylinResume} width="900px" height="970px" />
+      </div>
+    </section>
+  );
 };
 
-export default Resume; 
+export default Resume;
