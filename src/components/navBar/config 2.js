9aef9de1925b40c7deda7731766faf29f9a689bb
kaylin chung <kay1inchung@g.ucla.edu>
2024-07-17 22:53:11 -0700

    updated resume

---
+++ src/components/navBar/config 2.js
@@ -0,0 +1,22 @@
+export const navMenus = [
+    {
+        label: "HOME",
+        to : "/"
+    },
+    {
+        label: "ABOUT ME",
+        to : "/about"
+    },
+    {
+        label: "PORTFOLIO",
+        to : "/portfolio"
+    },
+    {
+        label: "RESUME",
+        to : "/resume"
+    },
+    {
+        label: "CONTACT ME",
+        to : "/contact"
+    },
+]
